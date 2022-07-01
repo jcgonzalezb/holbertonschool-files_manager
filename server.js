05b01a0e92741d8f944013fa28eb3ccdd2665ea1
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-30 20:27:16 -0500

    assigments fixed

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 import express from 'express';
-import indexRouter from './routes/index';
 
+const routes = require('./routes/index');
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-app.use('/', indexRouter);
-
+app.use(express.json());
+app.use('/', routes);
 module.exports = app;
